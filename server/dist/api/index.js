05c9a5f7deb2c171482a5771c58972a7ed7ff6ff
ad222kr <ad222kr@student.lnu.se>
2016-11-11 22:12:39 +0100

    deploy stuf

---
+++ server/dist/api/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _hapi = require('hapi');
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- server/dist/index.js
+++ server/dist/index.js
@@ -2,56 +2,18 @@
 
 var _hapi = require('hapi');
 
-var _inert = require('inert');
-
-var _inert2 = _interopRequireDefault(_inert);
-
-var _path = require('path');
-
-var _path2 = _interopRequireDefault(_path);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var environment = process.env.NODE_ENV || 'production';
-var isDevMode = environment === 'development';
 var server = new _hapi.Server();
 
 server.connection({
   port: 8000,
   routes: {
-    cors: environment === 'development',
-    files: isDevMode ? {} : {
-      relativeTo: _path2.default.join(__dirname, '../../ui/build')
-    }
+    cors: true
   }
 });
 
-if (!isDevMode) {
-  server.register(_inert2.default);
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: function handler(request, reply) {
-      reply.file(_path2.default.join(__dirname, '../../ui/build/index.html'));
-    }
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-      directory: {
-        path: '.',
-        redirectToSlash: true,
-        index: true
-      }
-    }
-  });
-}
-
 server.route({
   method: 'GET',
-  path: '/api/test',
+  path: '/',
   handler: function handler(request, reply) {
     reply('Hello');
   }
