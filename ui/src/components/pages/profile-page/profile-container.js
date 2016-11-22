c52c48bd17892e5f5c22b4ad39dc74a3a8c53ce4
hj222hi <hj222hi@student.lnu.se>
2016-11-22 17:09:33 +0100

    it prints out a user JSON

--- ui/src/components/pages/profile-page/profile-container.js
+++ ui/src/components/pages/profile-page/profile-container.js
@@ -2,21 +2,25 @@ import React, { PropTypes, Component } from 'react'
 import { Textfield, Button, Spinner } from 'react-mdl'
 import ProvileDisplayer from './profiledisplayer'
 
-class ProfileContainer extends Component {
 
+const user = {
+  email: 'test@test.com',
+  firstname: 'Hampus',
+  lastname: 'tester',
+}
+class ProfileContainer extends Component {
+  constructor(props){
+    super(props)
+
+    this.state={
+      user,
+    }
+  }
 
   render() {
-    let that = this;
-    that.setState( user = {
-      email: 'test@test.com',
-      firstname: 'Hampus',
-      lastname: 'tester',
-    })
-
-
     return (
       <div>
-        <ProvileDisplayer user ={this.state.user}/>
+        <ProvileDisplayer user={this.state.user}/>
       </div>
     )
   }
--- ui/src/components/pages/profile-page/profiledisplayer.js
+++ ui/src/components/pages/profile-page/profiledisplayer.js
@@ -1,19 +1,18 @@
 import React, { PropTypes, Component } from 'react'
-import { Textfield, Button, Spinner, Card, Grid, Cell } from 'react-mdl'
+import {  Card, Grid, Cell } from 'react-mdl'
 
 
 const ProvileDisplayer = (props) => {
-  const { onChange, onSubmit, email, password, passwordConfirm, isAuthenticating } = props
-
+  console.log(props)
   //There is an error right now with the fields, you can't write any text in them
   return (
     <Card shadow={0} style={{ margin: 'auto', width: '40%'}}>
       <Grid style={{width: '80%'}}>
-        <Cell col={10}><h2>Hampus <br />Jarleborn</h2></Cell>
-        <Cell col={10}><img src="http://hampusjarleborn.se/php_proj/data/6521141500846276608.png" style={{width: '150px'}}/></Cell>
+        <Cell col={10}><h2> {props.user.firstname} <br /> {props.user.lastname}</h2></Cell>
+        <Cell col={10}><img alt='tmppic' src="http://hampusjarleborn.se/php_proj/data/6521141500846276608.png" style={{width: '150px'}}/></Cell>
       </Grid>
       <Grid>
-        <Cell col={12}>Email: hampus@mail.com</Cell>
+        <Cell col={12}>Email: {props.user.email}</Cell>
       </Grid>
     </Card>
   )
