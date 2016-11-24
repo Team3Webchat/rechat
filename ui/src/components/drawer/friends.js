dd35f0a39e93d8e649dcb65d96d5e08fc00bd0bd
rf222cz <rf222cz@student.lnu.se>
2016-11-24 21:46:57 +0100

    Added contactChip with soon-to-be-profilepicture

--- ui/src/components/drawer/friends.js
+++ ui/src/components/drawer/friends.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Textfield, Button, Spinner, Drawer, Navigation, List, ListItem, Icon } from 'react-mdl'
+import { Textfield, Button, Spinner, Drawer, Navigation, List, ListItem, Icon, Chip, ChipContact } from 'react-mdl'
 
 const Friends = ({ friends, onFriendClick }) => {
 
   if (friends && friends.length > 0) {
     return (
+
       <List>
-        {friends.map(f => 
+        {friends.map(f =>
           <ListItem key={f.id}>
-            <span><Icon name="account_box"/>{f.firstname} {f.lastname}</span>
-          </ListItem> 
+            <Chip>
+                <ChipContact style={{ background: 'url("https://placekitten.com/150/150") 0 0 / cover' }}/>
+                {f.firstname} {f.lastname}
+            </Chip>
+          </ListItem>
         )}
       </List>
     )
   } else {
     return <p>No friends</p>
   }
-  
+
 }
 
 export default Friends
-
-
-
