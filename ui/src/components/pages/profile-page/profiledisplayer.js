cd10412acbbbfafcce96907d40435b2cb4245cff
hj222hi <hj222hi@student.lnu.se>
2016-11-29 15:58:09 +0100

    Added Gravatar for avatar

--- ui/src/components/pages/profile-page/profiledisplayer.js
+++ ui/src/components/pages/profile-page/profiledisplayer.js
@@ -1,6 +1,6 @@
 import React, { PropTypes, Component } from 'react'
 import {  Card, Grid, Cell, CardActions, Button, Icon } from 'react-mdl'
-
+import Gravatar from 'react-gravatar'
 
 const ProfileDisplayer = (props) => {
   console.log(props.user)
@@ -14,7 +14,7 @@ const ProfileDisplayer = (props) => {
         </Button>
       <Grid style={{width: '80%'}}>
         <Cell col={10}><h2> {name} </h2></Cell>
-        <Cell col={10}><img alt='tmppic' src="http://hampusjarleborn.se/php_proj/data/6521141500846276608.png" style={{width: '150px'}}/></Cell>
+        <Cell col={10}><Gravatar email={email} /></Cell>
       </Grid>
       <Grid>
         <Cell col={12}>Email: {email}</Cell>
