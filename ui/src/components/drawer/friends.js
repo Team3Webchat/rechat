af536e7d005e0492ac9634ed0cf07aa69e22c40b
rf222cz <rf222cz@student.lnu.se>
2016-11-30 13:42:54 +0100

    Fiends name to the confirm-dialog

--- ui/src/components/drawer/friends.js
+++ ui/src/components/drawer/friends.js
@@ -3,18 +3,17 @@ import { connect } from 'react-redux'
 import { List, ListItem, Chip, ChipContact } from 'react-mdl'
 import DeleteFriendConfirm from './delete-friend-confirm'
 
-const Friends = ({ friends, onFriendClick, doToggleDeleteFriend, startConversation }) => {
+const Friends = ({ friends, onFriendClick, deleteFriendConfirm, startConversation }) => {
   return (
     <List>
     {friends.length > 0 ?
     friends.map(f =>
       <ListItem key={f.id}>
         <Chip
-        onClose={e => doToggleDeleteFriend(f.id)}
-        onClick={e => startConversation()}>
-            <ChipContact
-            style={{ background: 'url("https://placekitten.com/150/150") 0 0 / cover' }}/>
-            {f.firstname} {f.lastname}
+        onClose={e => deleteFriendConfirm(f)}>
+          <ChipContact
+          style={{ background: 'url("https://placekitten.com/150/150") 0 0 / cover' }}/>
+          {f.firstname} {f.lastname}
         </Chip>
       </ListItem>
     )
@@ -24,8 +23,8 @@ const Friends = ({ friends, onFriendClick, doToggleDeleteFriend, startConversati
     </ListItem>
     }
     </List>
-    
+
   )
 }
-
+//onClick={e => startConversation()}>
 export default Friends
