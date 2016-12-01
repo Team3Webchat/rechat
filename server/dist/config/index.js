1091bf5ff2e16d394d01f39e6cbd36fd67646957
ad222kr <ad222kr@student.lnu.se>
2016-12-01 23:26:05 +0100

    deloy

--- server/dist/config/index.js
+++ server/dist/config/index.js
@@ -3,4 +3,4 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var CLIENT_URL = exports.CLIENT_URL = process.env.NODE_ENV === 'production' ? 'https://team3webchat.github.io/rechat' : 'http://localhost:3000';
+var CLIENT_URL = exports.CLIENT_URL = process.env.NODE_ENV === 'production' ? 'https://team3webchat.github.io' : 'http://localhost:3000';
