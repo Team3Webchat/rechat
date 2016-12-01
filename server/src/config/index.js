72cf8b88f5a4e389f28966fe719e0852757b282e
ad222kr <ad222kr@student.lnu.se>
2016-12-01 23:22:04 +0100

    added production url for origin so sockets work

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,1 +1,1 @@
-export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000'
+export const CLIENT_URL = process.env.NODE_ENV === 'production' ? 'https://team3webchat.github.io/rechat' : 'http://localhost:3000'
