6b9cd2438426c8321211707efa24d2f66d4753f4
rf222cz <rf222cz@student.lnu.se>
2016-12-07 10:40:29 +0100

    Small fixes in flash and chats in drawer

--- ui/src/lib/actions/flashActions.js
+++
@@ -1,8 +0,0 @@
-export const RESET_FLASH_MESSAGE = 'RESET_FLASH_MESSAGE'
-export const SET_FLASH_MESSAGE = 'SET_FLASH_MESSAGE'
-
-export const resetFlashMessage = () => ({
-  type: RESET_FLASH_MESSAGE,
-})
-
-
