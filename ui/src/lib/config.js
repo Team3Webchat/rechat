12087eda7d884f6a83d07168e70e235dce2f9c9f
ad222kr <ad222kr@student.lnu.se>
2016-12-12 21:52:38 +0100

    Setting env vars for ui

--- ui/src/lib/config.js
+++ ui/src/lib/config.js
@@ -1,4 +1,4 @@
-export const API_URL = getEnvVar('API_URL') || 'localhost:8000'
+export const API_URL = getEnvVar('REACT_APP_API_URL') || 'localhost:8000'
 
 export function getEnvVar(key) {
   return process.env[key]  
