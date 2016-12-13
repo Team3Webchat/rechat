398a417f0effa170a311627a2d9346eec967fb59
Åsa Axelsson <mattiasaxelsson@Mattiass-MacBook-Pro.local>
2016-12-13 05:43:26 +0100

    Added textfield for reporting person

--- ui/src/components/pages/profile-page/friend-profile-page/report-friend-confirm.js
+++ ui/src/components/pages/profile-page/friend-profile-page/report-friend-confirm.js
@@ -1,20 +1,37 @@
 import React from 'react'
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button} from 'react-mdl'
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Textfield} from 'react-mdl'
+
+import './../../style-dialog-common.css'
 
 const ReportFriendConfirm = (props) => {
   const { firstname, lastname, id } = props.friend
-  const {  handleCloseReportDialog, openReportDialog, handleReportFriend } = props
+  const {  onChange, onSubmit, handleCloseReportDialog, openReportDialog, handleReportFriend, reportDetails } = props
   return (
-    <Dialog open={openReportDialog}>
+    <Dialog open={openReportDialog} className="dialogMedium">
       <DialogTitle>Report this person</DialogTitle>
-        <DialogContent>
-          <p>Report {firstname} {lastname} for inappropriate behavior. 
-          The report will be submitted for review and our administration will look for violations of our Terms of use.</p>
+        <form onSubmit={onSubmit}>
+          <DialogContent>
+            <p>Report <span className="bold">{firstname} {lastname}</span> for inappropriate behavior. 
+            The report will be submitted for review and our administration will look for violations of our Terms of use.</p>
+            
+            <div>
+              <Textfield
+                className='textInput'
+                onChange={onChange}
+                label="Write details..."
+                required
+                multiLine={true}
+                rows={3}
+                value={reportDetails}
+              />
+            </div>
+
         </DialogContent>
-      <DialogActions>
-        <Button type='button' onClick={() => {handleReportFriend(id)}}>Report</Button>
-        <Button type='button' onClick={handleCloseReportDialog}>Cancel</Button>
+        <DialogActions className="dialogActionsBorder">
+          <Button raised colored disabled={reportDetails.length === 0} type="submit" onClick={() => {handleReportFriend(id)}}>Report</Button>
+          <Button type='button' onClick={handleCloseReportDialog}>Cancel</Button>
       </DialogActions>
+      </form>
     </Dialog>
   )
 }
